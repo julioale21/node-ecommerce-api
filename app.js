b46227a4db54c27b0529f00d6e79f55e2359f550
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-09-22 13:03:27 -0300

    added mongoose and conect database from mongodb atlas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 require('dotenv/config')
 
@@ -24,6 +25,15 @@ app.post(`${api}/products`, (req, res) => {
   console.log(newProduct)
   res.send(newProduct)
 })
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Database connection is ready')
+  })
+  .catch((err) => console.log(err))
 
 app.listen(3000, () => {
   console.log(api)
