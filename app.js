389316556c22660baee3228e912df4d8b6eba69a
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-09-22 12:05:08 -0300

    added dotenv dependency and .env file

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
+require('dotenv/config')
+
+const api = process.env.API_URL
+
+app.get(api + '/products', (req, res) => {
+  res.send('from products')
 })
 
 app.listen(3000, () => {
+  console.log(api)
   console.log('app running on http://localhost:3000')
 })
