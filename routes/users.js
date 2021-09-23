245eb2c3d9a60b600f1269f0642e30eef4e81710
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-09-23 12:58:58 -0300

    added create user

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,29 @@ router.get('/', async (req, res) => {
   res.status(200).send(userList)
 })
 
+router.post('/', async (req, res) => {
+  try {
+    let user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: req.body.passwordHash,
+      phone: req.body.phone,
+      isAdmin: req.body.isAdmin,
+      street: req.body.street,
+      apartment: req.body.apartment,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country
+    })
+
+    user = await user.save()
+    if (!user) {
+      res.status(400).json({ success: false, message: 'User could not be created.' })
+    }
+    res.status(201).send(user)
+  } catch (error) {
+    res.status(500).json({ success: false, error })
+  }
+})
+
 module.exports = router
