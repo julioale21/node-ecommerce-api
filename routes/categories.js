a8fb36af735ad16da03dcfcdd45ce2fd955263b1
Julio Romero <julioale21@gmail.com>
2021-09-22 22:07:04 -0300

    added find category by id

--- routes/categories.js
+++ routes/categories.js
@@ -7,7 +7,25 @@ router.get('/', async (req, res) => {
   if (!categoryList) {
     res.status(500).json({ success: false })
   }
-  res.send(categoryList)
+  res.status(200).send(categoryList)
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id)
+    if (!category) {
+      res.status(404).json({
+        success: false,
+        message: 'Category not found.'
+      })
+    }
+    res.status(200).send(category)
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error
+    })
+  }
 })
 
 router.post('/', async (req, res) => {
