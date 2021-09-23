b28b91df3512118537d0faeab17daa621aac5ec1
Julio Romero <julioale21@gmail.com>
2021-09-22 22:34:15 -0300

    updated create product

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../models/category')
 const Product = require('../models/product')
 
 router.get('/', async (req, res) => {
@@ -12,20 +13,44 @@ router.get('/', async (req, res) => {
   res.send(productList)
 })
 
-router.post('/', (req, res) => {
-  const product = new Product({
-    ...req.body
-  })
-  product.save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct)
-    })
-    .catch((error) => {
-      res.status(500).json({
-        error,
-        success: false
+router.post('/', async (req, res) => {
+  try {
+    const category = await Category.findById(req.body.category)
+    if (!category) {
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid Category'
       })
+    }
+
+    let product = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeature: req.body.isFeature
     })
+    product = await product.save()
+
+    if (!product) {
+      res.status(500).json({
+        success: false,
+        message: 'Product could not be created'
+      })
+    }
+    res.status(200).send(product)
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error
+    })
+  }
 })
 
 module.exports = router
