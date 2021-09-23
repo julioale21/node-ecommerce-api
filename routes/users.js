0acf8d7a25d6b372fd6dcdd8522ba8c6c333fc1d
Julio Romero <julioale21@gmail.com>
2021-09-23 19:53:29 -0300

    added bcrypt library and hashed user password

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 router.get('/', async (req, res) => {
   const userList = await User.find()
@@ -16,7 +17,7 @@ router.post('/', async (req, res) => {
     let user = new User({
       name: req.body.name,
       email: req.body.email,
-      passwordHash: req.body.passwordHash,
+      passwordHash: bcrypt.hashSync(req.body.password, 10),
       phone: req.body.phone,
       isAdmin: req.body.isAdmin,
       street: req.body.street,
