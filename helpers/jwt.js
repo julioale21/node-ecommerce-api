c1ab4cec3b3b6fb8394fff3a2e19206e43f59048
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-09-24 11:05:41 -0300

    exclude categories public routes from auth middleware

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,6 +9,7 @@ function authJwt () {
   }).unless({
     path: [
       { url: /\/api\/v1\/products(.*)/, method: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/categories(.*)/, method: ['GET', 'OPTIONS'] },
       `${api}/users/login`,
       `${api}/users/register`
     ]
