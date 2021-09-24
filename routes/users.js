b2f7d689c0ec415ef3e819cc4760b4cee3f5095c
Julio Romero <julioale21@gmail.com>
2021-09-23 23:17:14 -0300

    added token expiration

--- routes/users.js
+++ routes/users.js
@@ -57,7 +57,11 @@ router.post('/login', async (req, res) => {
   if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
     const token = jwt.sign({
       userId: user.id
-    }, secret)
+    },
+    secret,
+    {
+      expiresIn: '1d'
+    })
     res.status(200).send({ user: user.email, token })
   } else {
     res.status(400).send('Password is wrong')
