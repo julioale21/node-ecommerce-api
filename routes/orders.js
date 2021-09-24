0707380bc3909015d7b12052a41d65f8c9103040
Julio Romero <julioale21@gmail.com>
2021-09-24 18:07:38 -0300

    added populate to order products items and product category

--- routes/orders.js
+++ routes/orders.js
@@ -17,6 +17,12 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const order = await Order.findById(req.params.id)
     .populate('user', 'name')
+    .populate({
+      path: 'orderItems',
+      populate: {
+        path: 'product', populate: 'category'
+      }
+    })
 
   if (!order) {
     res.status(500).json({ success: false })
