979737961cf64bb6381109ee48a1ea7fb1c283dc
Julio Romero <julioale21@gmail.com>
2021-09-25 19:16:45 -0300

    added logic to manager upload images

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,34 @@ const router = express.Router()
 const Category = require('../models/category')
 const Product = require('../models/product')
 const mongoose = require('mongoose')
+const multer = require('multer')
+
+const FILE_TIP_MAP = {
+  'image/png': 'png',
+  'image/jp`g': 'jpg',
+  'image/jpeg': 'jpeg'
+}
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isValidFile = FILE_TIP_MAP[file.mimetype]
+    let uploadError = new Error('invlida image type')
+
+    if (isValidFile) {
+      uploadError = null
+    }
+    cb(uploadError, 'public/uploads')
+  },
+  filename: function (req, file, cb) {
+    let fileName = file.originalname.split(' ')
+      .join('-')
+
+    fileName = fileName.replace(`.${FILE_TIP_MAP[file.mimetype]}`, '')
+    const extension = FILE_TIP_MAP[file.mimetype]
+    cb(null, `${fileName}-${Date.now()}.${extension}`)
+  }
+})
+
+const uploadOptions = multer({ storage: storage })
 
 // api/v1/products?categories=3232,23123
 router.get('/', async (req, res) => {
@@ -30,18 +58,21 @@ router.get('/:id', async (req, res) => {
   res.send(product)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', uploadOptions.single('image'), async (req, res) => {
   try {
     const category = await Category.findById(req.body.category)
     if (!category) {
       return res.status(400).json({ success: false, message: 'Invalid Category' })
     }
 
+    const fileName = req.file.filename
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`
+
     let product = new Product({
       name: req.body.name,
       description: req.body.description,
       richDescription: req.body.richDescription,
-      image: req.body.image,
+      image: `${basePath}${fileName}`,
       brand: req.body.brand,
       price: req.body.price,
       category: req.body.category,
