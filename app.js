60fdf00b72c5ca3d0923efca67525e36d967b736
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-09-27 10:11:37 -0300

    updated app

--- app.js
+++ app.js
@@ -1,44 +1,52 @@
-/* eslint-disable node/no-path-concat */
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const productsRouter = require('./routes/products')
-const categoriesRouter = require('./routes/categories')
-const usersRouter = require('./routes/users')
-const ordersRouter = require('./routes/orders')
+require('dotenv/config')
 const authJwt = require('./helpers/jwt')
 const errorHandler = require('./helpers/error-handler')
-const path = require('path')
 
 app.use(cors())
 app.options('*', cors())
 
-require('dotenv/config')
-
-// Middlewares
+// middleware
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(authJwt())
+// eslint-disable-next-line node/no-path-concat
 app.use('/public/uploads', express.static(__dirname + '/public/uploads'))
 app.use(errorHandler)
 
+// Routes
+const categoriesRoutes = require('./routes/categories')
+const productsRoutes = require('./routes/products')
+const usersRoutes = require('./routes/users')
+const ordersRoutes = require('./routes/orders')
+
 const api = process.env.API_URL
 
-app.use(`${api}/products`, productsRouter)
-app.use(`${api}/categories`, categoriesRouter)
-app.use(`${api}/users`, usersRouter)
-app.use(`${api}/orders`, ordersRouter)
+app.use(`${api}/categories`, categoriesRoutes)
+app.use(`${api}/products`, productsRoutes)
+app.use(`${api}/users`, usersRoutes)
+app.use(`${api}/orders`, ordersRoutes)
 
-mongoose.connect(process.env.CONNECTION_STRING)
-  .then(() => {
-    console.log('Database connected')
+// Database
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.DB_NAME
   })
-  .catch((err) => console.log(err))
-
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(api)
-  console.log('app running on http://localhost:3000')
+  .then(() => {
+    console.log('we are using ' + process.env.DB_NAME)
+    console.log('Database Connection is ready...')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+const PORT = process.env.PORT || 3000
+// Server
+app.listen(PORT, () => {
+  console.log('server is running http://localhost:3000')
 })
