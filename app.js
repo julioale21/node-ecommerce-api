f82b98c345ee1570f46f5757743ff251fa2d0bec
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-09-27 10:06:46 -0300

    updated app

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ require('dotenv/config')
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(authJwt())
-app.use('/public/uploads', express.static(path.resolve(__dirname + '/public/uploads')))
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'))
 app.use(errorHandler)
 
 const api = process.env.API_URL
@@ -31,11 +31,7 @@ app.use(`${api}/categories`, categoriesRouter)
 app.use(`${api}/users`, usersRouter)
 app.use(`${api}/orders`, ordersRouter)
 
-mongoose.connect(process.env.CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: process.env.DB_NAME
-})
+mongoose.connect(process.env.CONNECTION_STRING)
   .then(() => {
     console.log('Database connected')
   })
